3d7fdbf07f5f20102ce531c29c2680d8570f44ce
tetete2016 <tetetelego2013@gmail.com>
2016-07-08 17:52:29 +0900

    first test

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static('public'));
+
+var colors = [0, 0, 0];
+
+app.get('/getcolor', function (request, response) {
+    if (req.body.id) {
+        response.send(JSON.stringify(colors[req.body.id]));
+    } else {
+        response.send("0");
+    }
+});
+app.get('/setcolor', function (request, response) {
+    if (req.body.id && req.body.color) {
+        colors[req.body.id] = req.body.color;
+    }
+    response.send("sent!!");
+});
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
