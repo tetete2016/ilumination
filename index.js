759a19733d1e064d3ba25e43a5953be07ab20cc8
tetete2016 <tetetelego2013@gmail.com>
2016-07-11 18:34:10 +0900

    used query

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var colors = [0, 0, 0];
 
 app.get('/getcolor', function (request, response) {
     console.log(request.query);
-    var q = JSON.parse(request.query);
-    console.log(q);
-    if (q.id != null) {
-        var index = Number.parseInt(q.id, 10);
+    if (request.query.id != null) {
+        var index = Number.parseInt(request.query.id, 10);
         if (0 <= index && index < colors.length) {
             response.send(colors[index]);
         }
@@ -21,14 +22,11 @@ app.get('/getcolor', function (request, response) {
 });
 app.get('/setcolor', function (request, response) {
     console.log(request.query);
-    var q = JSON.parse(request.query);
-    console.log(q);
-    if (q.id != null && q.color != 0) {
-        var index = Number.parseInt(q.id, 10);
-        if (index) {
-            colors[index] = q.color;
-        }
+    if (request.query.id != null && request.query.color != null) {
+        var index = Number.parseInt(request.query.id, 10);
+        colors[index] = request.query.color;
     }
+    response.send("sent");
 });
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
