d37204277924cf5ca360125366c4f6c92605c748
tetete2016 <tetetelego2013@gmail.com>
2016-08-13 09:28:17 +0900

    upload

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/getcolor', function (request, response) {
         console.log(index);
         console.log(colors);
         if (0 <= index && index < colors.length) {
-            response.send(colors[index]);
+            response.send(index);
         }
     }
     response.send("0");
