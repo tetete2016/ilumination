d7adaad743ca26d8b259212dd87c7020d2395ca8
tetete2016 <tetetelego2013@gmail.com>
2016-08-14 22:09:07 +0900

    added animation

--- public/TeteteAjax.js
+++ public/TeteteAjax.js
@@ -20,7 +20,7 @@ function doget(data, url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.onreadystatechange = function () {
+    xhr.onload = function () {
         console.log("callback doget-2" + "state" + xhr.readyState + "status" + xhr.status);
         if ((xhr.readyState === 4 && xhr.status === 0) || (xhr.readyState === 4 && xhr.status === 200)) {
             console.log("callback doget-1");
